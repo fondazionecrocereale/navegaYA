b78cbbf29719ed7d3b6b8bcfa6dfcf5efd967fb4
fondazionecrocereale <fondazionecrocereale@gmail.com>
2024-08-07 20:17:26 -0300

    second commit

--- db/connection.go
+++ db/connection.go
@@ -13,12 +13,12 @@ const (
 	host     = "private-db-postgresql-nyc3-68472-do-user-17449050-0.m.db.ondigitalocean.com"
 	port     = 25060
 	user     = "doadmin"
-	password = "AVNS_0RVpY_d_VjNV9QF5sTe"
+	contra = "AVNS_0RVpY_d_VjNV9QF5sTe"
 	dbName   = "defaultdb"
 )
 
 func DBconnection() *gorm.DB {
-	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", host, port, user, password, dbName)
+	sqlInfo := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=require", host, port, user, contra, dbName)
 	db, err := gorm.Open(postgres.Open(sqlInfo), &gorm.Config{})
 	helper.ErrorPanic(err)
 
